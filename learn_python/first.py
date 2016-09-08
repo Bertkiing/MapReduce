c7524f36167a9d11bcbc764bd2a2040a7c57bf72
KING\King <kingcheeng163@gmail.com>
2016-09-09 00:09:29 +0800

    Python IO

---
+++ learn_python/first.py
@@ -0,0 +1,17 @@
+100+200+300
+print(1+2+3)
+print("hello,python")
+print("first","second","third","fourth","fifth","sixth","seventh","eighth","ninth")
+print("500+1=",500+1)
+
+"""
+ÊäÈë ĞÕÃû
+"""
+name=input("Please enter your name:")
+print("name =",name)
+
+
+"""
+Êä³ö1024*768
+"""
+print("1024*768 =",1024*768)
