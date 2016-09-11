b23827defaa80c236cd0d12e3a4caba5783b2455
KING\King <kingcheeng163@gmail.com>
2016-09-12 00:41:22 +0800

    learning python确实是一本好书啊

---
+++ learn_python/script.py
@@ -0,0 +1,3 @@
+print ("hello,python")
+print("2+2=",2+2)
+print("2 ** 100 = ",2**100)
---
+++ learn_python/script_1.py
@@ -0,0 +1,6 @@
+# A first Python script
+import sys  #Load a library module
+print(sys.platform)  
+print(2**8)  # Raise 2 to s power
+x = "Spam!"
+print(x*8) #String repetition
